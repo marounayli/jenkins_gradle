3f77d190937eff4285fe87e0c6e01c4b48c3feac
Maroun Ayli <maroun.ayli@foo-solutions.com>
2021-09-23 23:59:50 +0300

    first commit

---
+++ src/main/java/com/gralde_jenkins/gradle_jenkins/GradleJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.gralde_jenkins.gradle_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gralde_jenkins/gradle_jenkins/GradleJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gralde_jenkins.gradle_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
